fb1eea709a7fedfae8f063468e4d458e11f5e4b5
MuhammadAsim786786 <asim.leo3243@gmail.com>
2025-02-28 11:37:40 +0500

    Optimized Images

--- app/components/ProjectShowcase/ProjectShowcase.tsx
+++ app/components/ProjectShowcase/ProjectShowcase.tsx
@@ -19,7 +19,7 @@ interface Testimonial {
 // Testimonials data
 const testimonials: Testimonial[] = [
   {
-    image: "assets/ProjectShowcase/1st.png",
+    image: "assets/ProjectShowcase/1st.webp",
     text: `"The Wild Oasis customer app is a user-friendly platform built with Next.js, allowing users to explore and book luxury cabins effortlessly. Customers can manage their profiles, track past, current, and future reservations, and enjoy a seamless browsing experience with a modern design."`,
     author: "Mari de Almedia",
     liveLink: "https://the-wild-quarry-oasis-customer-website.vercel.app/",
@@ -27,14 +27,14 @@ const testimonials: Testimonial[] = [
       "https://github.com/MuhammadAsim786786/the-wild-quarry-oasis-customer-website",
   },
   {
-    image: "assets/ProjectShowcase/pth.png",
+    image: "assets/ProjectShowcase/pth.webp",
     text: `"The Wild Oasis business app is a robust React-based tool designed for managing and monitoring cabin rentals and business operations. It offers features like tracking bookings (past, current, and future), monitoring sales, managing active cabins, and handling user profiles."`,
     author: "Mari de Almedia",
     liveLink: "https://the-wild-quarry-oasis.vercel.app",
     githubLink: "https://github.com/MuhammadAsim786786/the-wild-quarry-oasis",
   },
   {
-    image: "assets/ProjectShowcase/2nd.png",
+    image: "assets/ProjectShowcase/2nd.webp",
     text: `"World Wise is an interactive website that empowers users to explore a world map and visually mark the places they've visited.A world map that tracks your footsteps into every city you can think of. Never forget your wonderful experiences, and show your friends how you have wandered the world."`,
     author: "Mari de Almedia",
     liveLink: "https://worldwise-happy-travelling.netlify.app/",
@@ -42,7 +42,7 @@ const testimonials: Testimonial[] = [
       "https://github.com/MuhammadAsim786786/Fast-React-Pizza-Application",
   },
   {
-    image: "assets/ProjectShowcase/3rd.png",
+    image: "assets/ProjectShowcase/3rd.webp",
     text: `"Fast React Pizza is a sleek and responsive web application built with React, offering a seamless online pizza ordering experience. Customers can browse a variety of pizza options, customize their orders, and add items to their cart with ease. Designed for speed and simplicity."`,
     author: "John Doe",
     liveLink: "https://fast-react-instant-pizza.netlify.app",
@@ -50,14 +50,14 @@ const testimonials: Testimonial[] = [
       "https://github.com/MuhammadAsim786786/Fast-React-Pizza-Application",
   },
   {
-    image: "assets/ProjectShowcase/4th.png",
+    image: "assets/ProjectShowcase/4th.webp",
     text: `Omnifood is a website that is entirely build on vanilla HTML and CSS.Omnifood provides a smart 365-days-per-year food subscription that will make you eat healthy again. Tailored to your personal tastes and nutritional needs.A healthy meal is delivered to you every single day"`,
     author: "John Doe",
     liveLink: "https://asim-omnifood-c.netlify.app/",
     githubLink: "https://github.com/MuhammadAsim786786/Omnifood",
   },
   {
-    image: "assets/ProjectShowcase/5th.png",
+    image: "assets/ProjectShowcase/5th.webp",
     text: `"Use popcorn is React-based web application designed to provide a user-friendly experience for movie enthusiasts. Users can search for movies, view detailed information, and rate their favorites. The platform allows users to store their ratings and movie preferences."`,
     author: "John Doe",
     liveLink: "https://usepopcorn-search-movies.netlify.app/",
